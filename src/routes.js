2c9bc05e4fcb19c65f6c59233e7a53dd267ae957
tharinduEranga <tharindueranga3@gmail.com>
2021-02-13 17:41:58 +0530

    add files to git

---
+++ src/routes.js
@@ -0,0 +1,78 @@
+/*!
+
+=========================================================
+* Now UI Dashboard React - v1.4.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/now-ui-dashboard-react
+* Copyright 2020 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/now-ui-dashboard-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import Dashboard from "views/Dashboard.js";
+import Notifications from "views/Notifications.js";
+import Icons from "views/Icons.js";
+import Typography from "views/Typography.js";
+import TableList from "views/TableList.js";
+import UserPage from "views/UserPage.js";
+import Products from "views/Products";
+
+const dashRoutes = [
+  {
+    path: "/dashboard",
+    name: "Dashboard",
+    icon: "design_app",
+    component: Dashboard,
+    layout: "/admin",
+  },
+  {
+    path: "/icons",
+    name: "Icons",
+    icon: "design_image",
+    component: Icons,
+    layout: "/admin",
+  },
+  {
+    path: "/notifications",
+    name: "Notifications",
+    icon: "ui-1_bell-53",
+    component: Notifications,
+    layout: "/admin",
+  },
+  {
+    path: "/user-page",
+    name: "User Profile",
+    icon: "users_single-02",
+    component: UserPage,
+    layout: "/admin",
+  },
+  {
+    path: "/extended-tables",
+    name: "Table List",
+    icon: "files_paper",
+    component: TableList,
+    layout: "/admin",
+  },
+  {
+    path: "/products",
+    name: "Products List",
+    icon: "shopping_box",
+    component: Products,
+    layout: "/admin",
+  },
+  {
+    path: "/typography",
+    name: "Typography",
+    icon: "design-2_ruler-pencil",
+    component: Typography,
+    layout: "/admin",
+  },
+
+];
+export default dashRoutes;
