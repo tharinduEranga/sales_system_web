cc689cede1794716f788a29b55602c36fc78789e
tharinduEranga <tharindueranga3@gmail.com>
2021-02-21 19:57:27 +0530

    added make stock request and validation

--- src/views/MakeStockRequest.js
+++ src/views/MakeStockRequest.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 import PanelHeader from "components/PanelHeader/PanelHeader.js";
-import {Card, CardBody, CardHeader, CardTitle, Col, Form, Row} from "reactstrap";
+import {Card, CardBody, CardHeader, CardTitle, Col, Form, FormGroup, Row} from "reactstrap";
 import {MDBBtn, MDBDataTableV5} from 'mdbreact';
 import axios from "axios";
 import {SERVER_URL_DEV} from "../variables/constants";
@@ -17,17 +17,29 @@ class MakeStockRequest extends React.Component {
         branchesUrl: SERVER_URL_DEV.concat(`/branch`),
         stockRequestsUrl: SERVER_URL_DEV.concat(`/stock-request`),
         productsUrl: SERVER_URL_DEV.concat(`/product`),
-        stockRequest: {
-            id: '',
-            byBranchId: '',
-            forBranchId: '',
-            vehicleId: ''
-        },
         stockRequestErrors: {
             byBranchId: '',
             forBranchId: '',
             vehicleId: '',
-            qty: 0
+            productId: '',
+            qty: 0,
+            products: ''
+        },
+        productAddData: {
+            forBranchId: '43242324',
+            byBranchId: '',
+            qty: '',
+            productId: ''
+        },
+        productAddDataErrors: {
+            byBranchId: '',
+            qty: '',
+            productId: ''
+        },
+        productDetailsTableTmp: {
+            productId: '',
+            productName: '',
+            qty: ''
         },
         addedProductDetailsTable: {
             columns: [
@@ -47,18 +59,25 @@ class MakeStockRequest extends React.Component {
             rows: []
         },
         branches: [<option key='' value=''>(Select a from branch)</option>],
-        selectedByBranchId: '',
-        insertedQty: 0,
+        products: [<option key='' value=''>(Select a from product)</option>],
         processing: false
     }
 
     addValidateSchema = {
         byBranchId: Joi.string().required(),
-        forBranchId: Joi.string().required()
+        forBranchId: Joi.string().required(),
+    }
+
+    addDataValidateSchema = {
+        forBranchId: Joi.string().required(),
+        byBranchId: Joi.string().required(),
+        productId: Joi.string().required(),
+        qty: Joi.number().required(),
     }
 
     async componentDidMount() {
-        return this.setBranches();
+        this.setBranches();
+        this.setProducts();
     }
 
     render() {
@@ -79,16 +98,16 @@ class MakeStockRequest extends React.Component {
                                         <div className="col-12">
                                             <div className="d-flex align-items-end justify-content-end">
                                                 <div className="container">
-                                                    <Form onSubmit={this.addStockRequest}>
+                                                    <Form onSubmit={this.addToSelectedClick}>
                                                         <Row className="justify-content-center">
                                                             <Col className="pr-1" md="12">
                                                                 <InputSelect
-                                                                    label=""
-                                                                    id="selectBranchId"
-                                                                    name="selectBranchId"
-                                                                    error={this.state.stockRequestErrors.byBranchId}
-                                                                    value={this.state.selectedByBranchId}
-                                                                    onChange={this.handleSelectByBranchChange}
+                                                                    label="Branch"
+                                                                    id="byBranchId"
+                                                                    name="byBranchId"
+                                                                    error={this.state.productAddDataErrors.byBranchId}
+                                                                    value={this.state.productAddData.byBranchId}
+                                                                    onChange={this.handleAddFormChange}
                                                                     options={
                                                                         this.state.branches
                                                                     }
@@ -96,18 +115,32 @@ class MakeStockRequest extends React.Component {
                                                                 </InputSelect>
                                                             </Col>
                                                             <Col className="pr-1" md="12">
+                                                                <InputSelect
+                                                                    label="Product"
+                                                                    id="productId"
+                                                                    name="productId"
+                                                                    error={this.state.productAddDataErrors.productId}
+                                                                    value={this.state.productAddData.productId}
+                                                                    onChange={this.handleAddFormChange}
+                                                                    options={
+                                                                        this.state.products
+                                                                    }
+                                                                >
+                                                                </InputSelect>
+                                                            </Col>
+                                                            <Col className="pr-1" md="12">
                                                                 <InputNumber
-                                                                    label="Price Per One"
-                                                                    id="price"
-                                                                    name="price"
-                                                                    error={this.state.stockRequestErrors.qty}
-                                                                    value={this.state.insertedQty}
+                                                                    label="QTY"
+                                                                    id="qty"
+                                                                    name="qty"
+                                                                    error={this.state.productAddDataErrors.qty}
+                                                                    value={this.state.productAddData.qty}
                                                                     onChange={this.handleAddFormChange}
                                                                 />
                                                             </Col>
                                                             <Col md="12">
-                                                                <Button type="submit" variant="primary" block
-                                                                        disabled={this.state.processing}>Save</Button>
+                                                                <Button type="submit" variant="primary" block>
+                                                                    Save</Button>
                                                             </Col>
                                                         </Row>
                                                     </Form>
@@ -118,12 +151,21 @@ class MakeStockRequest extends React.Component {
 
                                     <hr/>
                                     <div className="container">
+                                        {this.state.stockRequestErrors.products ?
+                                            <div className="alert alert-danger">
+                                                {this.state.stockRequestErrors.products}</div> : ''}
+
                                         <MDBDataTableV5 hover entriesOptions={[5, 20, 25]}
                                                         entries={5}
                                                         data={this.state.addedProductDetailsTable}
                                                         fullPagination
                                                         searchTop searchBottom={false}/>
                                     </div>
+                                    <Col md="12">
+                                        <Button type="submit" variant="primary" block
+                                                disabled={this.state.processing}
+                                                onClick={this.addStockRequest}>Add Stock Request</Button>
+                                    </Col>
                                 </CardBody>
                             </Card>
                         </Col>
@@ -145,33 +187,78 @@ class MakeStockRequest extends React.Component {
         });
     }
 
-    handleAddFormChange = ({currentTarget: input}) => {
-        const addStockRequest = {...this.state.stockRequest};
-        addStockRequest[input.name] = input.value;
-        this.setState({addStockRequest});
+    async setProducts() {
+        const response = await axios.get(this.state.productsUrl);
+        let productOptions = response.data.data.map(product => {
+            return <option key={product.id} value={product.id}>{product.name}</option>
+        });
+        productOptions = [<option key='' value=''>(Select a product)</option>, ...productOptions];
+
+        this.setState({
+            products: productOptions
+        });
     }
 
-    handleSelectByBranchChange = async ({currentTarget: input}) => {
-        let selectedByBranchId = input.value;
-        await this.setState({selectedByBranchId});
+    handleAddFormChange = ({currentTarget: input}) => {
+        const productAddData = {...this.state.productAddData};
+        productAddData[input.name] = input.value;
+
+        let productId = '';
+        let productName = '';
+        let qty = '';
+        if (input.name === 'productId') {
+            productId = input.value;
+            productName = input.selectedOptions[0].text;
+            qty = this.state.productDetailsTableTmp.qty;
+        } else if (input.name === 'qty') {
+            productId = this.state.productDetailsTableTmp.productId;
+            productName = this.state.productDetailsTableTmp.productName;
+            qty = input.value;
+        }
+
+        this.setState({
+            productDetailsTableTmp: {
+                productId: productId,
+                productName: productName,
+                qty: qty
+            }
+        });
+
+        this.setState({productAddData});
     }
 
     addStockRequest = async event => {
         event.preventDefault();
-        const stockRequestErrors = this.addFormErrors();
+        const stockRequestErrors = this.makeRequestErrors();
         this.setState({stockRequestErrors});
+
         if (Object.keys(stockRequestErrors).length > 0)
             return;
+
+        const makeRequestData = {
+            forBranchId: this.state.productAddData.forBranchId,
+            byBranchId: this.state.productAddData.byBranchId,
+            stockRequestDetails: this.state.addedProductDetailsTable.rows.map(addedProduct => ({
+                productId: addedProduct.productId,
+                qty: addedProduct.qty
+            }))
+        };
 
         this.setProcessing(true);
 
         try {
-            const response = await axios.post(this.state.stockRequestsUrl, this.state.stockRequest);
+            const response = await axios.post(this.state.stockRequestsUrl, makeRequestData);
             if (response.data.success) {
                 Functions.successSwal(response.data.message);
                 this.closeAddModal();
-                this.setState({addStockRequest: {name: ''}});
-                await this.setBranches();
+                this.setState({
+                    stockRequest: {
+                        id: '',
+                        byBranchId: '',
+                        forBranchId: '',
+                        vehicleId: ''
+                    }
+                });
             }
         } catch (e) {
         }
@@ -179,11 +266,28 @@ class MakeStockRequest extends React.Component {
         this.setProcessing(false);
     }
 
+    addToSelectedClick = (event) => {
+        event.preventDefault();
+        const productAddDataErrors = this.addFormErrors();
+        console.log(productAddDataErrors);
+        this.setState({productAddDataErrors});
+        if (Object.keys(productAddDataErrors).length > 0)
+            return;
+        let addedProductDetailsTable = this.state.addedProductDetailsTable;
+        const newAddedProducts = [this.state.productDetailsTableTmp, ...addedProductDetailsTable.rows];
+        this.setState({
+            addedProductDetailsTable: {
+                columns: addedProductDetailsTable.columns,
+                rows: newAddedProducts
+            }
+        });
+    }
+
     addFormErrors = () => {
         const errors = {};
-        const {addStockRequest} = this.state;
+        const {productAddData} = this.state;
         const options = {abortEarly: false};
-        let validate = Joi.validate(addStockRequest, this.addValidateSchema, options);
+        let validate = Joi.validate(productAddData, this.addDataValidateSchema, options);
 
         if (!validate.error) return errors;
 
@@ -192,18 +296,24 @@ class MakeStockRequest extends React.Component {
         return errors;
     }
 
-    productUpdateClick = (event) => {
-        const selected = JSON.parse(event.target.value);
+    makeRequestErrors = () => {
+        const errors = {};
+        const stateErrors = this.state.stockRequestErrors;
+        const addedData = this.state.addedProductDetailsTable.rows;
+        if (addedData.length <= 0)
+            errors.products = 'Please add at least one product';
+
         this.setState({
-            updateBranch: {
-                id: selected.id,
-                name: selected.name,
-                address: selected.address,
-                tel: selected.tel,
-                statusId: selected.statusId
+            stockRequestErrors: {
+                byBranchId: stateErrors.byBranchId,
+                forBranchId: stateErrors.forBranchId,
+                vehicleId: stateErrors.vehicleId,
+                productId: stateErrors.productId,
+                qty: stateErrors.qty,
+                products: errors.products
             }
         });
-        this.openUpdateModal();
+        return errors;
     }
 
     setProcessing = (processing) => {
