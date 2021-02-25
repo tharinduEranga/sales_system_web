d63b27c8a9c76acde1fa3db5c01d3d3361817a4c
tharinduEranga <tharindueranga3@gmail.com>
2021-02-25 20:10:54 +0530

    added log out button and redirect

--- src/components/Navbars/DemoNavbar.js
+++ src/components/Navbars/DemoNavbar.js
@@ -104,6 +104,10 @@ class Header extends React.Component {
       });
     }
   };
+  constructor(props) {
+    super(props);
+    this.logOut = this.logOut.bind(this);
+  }
   componentDidMount() {
     window.addEventListener("resize", this.updateColor.bind(this));
   }
@@ -160,55 +164,30 @@ class Header extends React.Component {
             navbar
             className="justify-content-end"
           >
-            <form>
-              <InputGroup className="no-border">
-                <Input placeholder="Search..." />
-                <InputGroupAddon addonType="append">
-                  <InputGroupText>
-                    <i className="now-ui-icons ui-1_zoom-bold" />
-                  </InputGroupText>
-                </InputGroupAddon>
-              </InputGroup>
-            </form>
+
             <Nav navbar>
-              <NavItem>
-                <Link to="#pablo" className="nav-link">
-                  <i className="now-ui-icons media-2_sound-wave" />
-                  <p>
-                    <span className="d-lg-none d-md-block">Stats</span>
-                  </p>
-                </Link>
-              </NavItem>
+
               <Dropdown
                 nav
                 isOpen={this.state.dropdownOpen}
                 toggle={(e) => this.dropdownToggle(e)}
               >
                 <DropdownToggle caret nav>
-                  <i className="now-ui-icons location_world" />
-                  <p>
-                    <span className="d-lg-none d-md-block">Some Actions</span>
-                  </p>
+                  <i className="now-ui-icons users_single-02" />
                 </DropdownToggle>
                 <DropdownMenu right>
-                  <DropdownItem tag="a">Action</DropdownItem>
-                  <DropdownItem tag="a">Another Action</DropdownItem>
-                  <DropdownItem tag="a">Something else here</DropdownItem>
+                  <DropdownItem tag="a" href="#" onClick={this.logOut}>Log Out</DropdownItem>
                 </DropdownMenu>
               </Dropdown>
-              <NavItem>
-                <Link to="#pablo" className="nav-link">
-                  <i className="now-ui-icons users_single-02" />
-                  <p>
-                    <span className="d-lg-none d-md-block">Account</span>
-                  </p>
-                </Link>
-              </NavItem>
             </Nav>
           </Collapse>
         </Container>
       </Navbar>
     );
+  }
+
+  logOut() {
+    this.props.history.push('/login');
   }
 }
 
